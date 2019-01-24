87490aa4e362aadf2583b4f63f5ed2bc3fccd6d2
Tilman M. Jaeschke <zwergnase@posteo.de>
2019-01-24 12:48:28 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# FlexGrid
+A css library for responsive webdesign
+
+## Usage
+```
+cd <webserver_root_folder>
+git clone https://github.com/asciibeats/flexgrid.git
+cd flexgrid
+npm install
+grunt
+```
+then open `http://<webserver_domain_name>/flexgrid/dist/index.html` in your favorite browser.
---
+++ gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				options: {
+					loadPath: 'node_modules/normalize-scss/sass',
+					style: 'compact',
+					sourcemap: 'none'
+				},
+				files: {'build/<%= pkg.name %>.css': ['src/styles/index.scss']}
+			}
+		},
+		autoprefixer: {
+			options: {
+			},
+			dist: {
+				files: {'build/<%= pkg.name %>.css': ['build/<%= pkg.name %>.css']}
+			}
+		},
+		cssmin: {
+			dist: {
+				options: {},
+				files: {'dist/<%= pkg.name %>.min.css': 'build/<%= pkg.name %>.css'}
+			}
+		},
+		htmlmin: {
+			dist: {
+				options: {removeComments: true, collapseWhitespace: true},
+				files: [{expand: true, flatten: true, src: 'src/**/*.html', dest: 'dist/'}]
+			}
+		},
+		watch: {
+			options: {livereload: true},
+			sass: {
+				files: ['src/**/*.scss'],
+				tasks: ['sass', 'autoprefixer', 'cssmin']
+			},
+			html: {
+				files: ['src/**/*.html'],
+				tasks: ['htmlmin']
+			}
+		},
+		copy: {
+			images: {
+				files: [{expand: true, flatten: true, src: 'src/images/**/*', dest: 'dist/images/'}]
+			},
+			/*bower_components: {
+				files: {
+					'dist/normalize.css': 'bower_components/normalize.css/normalize.css'
+				}
+			}*/
+		},
+		clean: {
+			dist: ["dist/", "build/", ".sass-cache"]
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+
+	grunt.registerTask('default', ['sass', 'autoprefixer', 'cssmin', 'htmlmin', 'copy', 'watch']);
+	grunt.registerTask('pure', ['sass', 'cssmin', 'htmlmin', 'copy', 'watch']);
+};
